d1f3068d2a1c462a902dfddbab2a1c004c26fdf2
UDAY BAG <uday625@Nemesis-2.local>
2018-11-24 22:52:58 -0500

    form alignment changes

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -7,7 +7,7 @@ const TodoForm = props => {
             <div className="TodoFormMain">
                 <form onSubmit= {props.addTodo}>
                     <div className="form-group row">
-                        <div className="col-sm-10 col-8">
+                        <div className="col-sm-10 col-6">
                             <input className="form-control"
                                 type="text" 
                                 placeholder="+ Add a to-do..."
@@ -15,7 +15,7 @@ const TodoForm = props => {
                                 onChange ={props.changetodo}>
                             </input>
                         </div>
-                        <div className ="col-sm-2 col-2">
+                        <div className ="col-sm-2 col-6">
                             <button type="submit" className="btn btn-warning"> Add Task</button>
                         </div>
                     </div>
